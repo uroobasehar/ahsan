93127201bdbaf7b8758e82c571c59513ecbb706e
uroobasehar <uroobasehar191@gmail.com>
2020-04-28 15:17:43 +0500

    updated

--- app/models.py
+++ app/models.py
@@ -19,6 +19,8 @@ class product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
+    product_img = models.ImageField() 
+
     
     class Meta:
         db_table = 'product'
